e33a84c5ad8089fe985f86dc15958f91c4f0f321
saksit <dewbbgun@hotmail.com>
2021-04-02 23:44:40 +0700

    working with date

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -3,6 +3,7 @@ package users
 import (
 	"fmt"
 
+	"github.com/gunbbdew123/bookstore_users-api/utils/date_utils"
 	"github.com/gunbbdew123/bookstore_users-api/utils/errors"
 )
 
@@ -35,6 +36,8 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exist", user.Id))
 	}
+	user.DateCreated = date_utils.GetNowString()
+
 	userDB[user.Id] = user
 	return nil
 }
