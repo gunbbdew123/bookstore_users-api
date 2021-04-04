b625e7f00ccbd2ede909c23ef69139c074f52b22
saksit <dewbbgun@hotmail.com>
2021-04-04 23:27:35 +0700

    config MySQL database

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -3,6 +3,7 @@ package users
 import (
 	"fmt"
 
+	"github.com/gunbbdew123/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/gunbbdew123/bookstore_users-api/utils/date_utils"
 	"github.com/gunbbdew123/bookstore_users-api/utils/errors"
 )
@@ -14,6 +15,10 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
+
 	result := userDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
